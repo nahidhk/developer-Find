25afe70ff45857bf33493b0d5e5793c8b28870be
nahidappltd@gmail.com <nahidappltd@gmail.com>
2024-02-02 18:12:53 +0600

    new

--- javascript/app.js
+++ javascript/app.js
@@ -71,3 +71,33 @@ function windocall(){
   window.location.href="aduser";
 }
 
+
+
+
+
+
+
+
+
+
+document.write(`
+<div style="position:fixed;z-index:100;bottom:20px;right:20px;height:66px;width:70px;background-color:#232323;border-radius:10px;">
+  
+  <a href="https://github.com/nahidhk/developer-Find">
+    
+   <i style="font-size: 50px;color: white;padding: 10px;" class="bi bi-github"></i> 
+  </a>
+  
+  </div>
+`);
+
+
+
+
+
+
+
+
+
+
+
